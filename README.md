8c4cc437ff34a4a878adf98516141963d80b8535
Malachi Gray <graymd.cfi@gmail.com>
2015-01-15 14:33:48 -0500

    currently has the ability to create new blog entries and delete them.  working on editing a previously created blog

---
+++ README.md
@@ -0,0 +1,9 @@
+Blog Website Using Sinatra and Databases
+
+Files required:
+1) .rvmrc -> contains ruby version number (2.1.5) required to run project.  Also creates the ruby gemset for the project.
+2) Gemfile -> contains the gems utilized in the project from the source page https://rubygems.org: 'sinatra', 'mysql', 'data_mapper', 'dm-mysql-adapter'
+3) Gemfile.lock -> autocreated from gemfile to resolve dependencies for gems required in Gemfile
+4) blog_server.rb -> main server page for the blog site
+
+
--- blog_server.rb
+++ blog_server.rb
@@ -1,2 +1,87 @@
 require 'sinatra'
 require 'data_mapper'
+
+DataMapper.setup(
+	:default,
+	'mysql://root@localhost/blog_data'
+	)
+
+
+class Blog
+	include DataMapper::Resource
+	property :id, Serial
+	property :title, String
+	property :date, String
+	property :writeup, String	
+end
+
+DataMapper.finalize.auto_upgrade!
+
+#Index
+get '/' do
+	@page_title = "My Blog"
+	@blogs= Blog.all
+	erb :index
+end
+
+#New
+get '/new' do
+	erb :create_new_blog_entry
+end
+
+#Create
+post '/create_blog' do
+	@blog = Blog.new
+	@blog.title = params[:title]
+	@blog.date = params[:date]
+	@blog.writeup = params[:writeup]
+	@blog.save
+	redirect to '/'
+end
+
+#Show
+get '/blog/:id' do
+	@blog = Blog.get params[:id]
+	erb :display_blog
+end
+
+#Delete
+delete '/delete_blog/:id' do
+	@blog = Blog.get params[:id]
+	@blog.destroy
+	redirect to '/'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
